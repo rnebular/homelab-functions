c640039a72945549ab8648bf93fba3ea1424a689
Jon Byrum <jtbyrum@gmail.com>
2026-03-08 00:45:56 -0800

    file name should have log file name at the end to allow file type to remain LOG

--- source/general-functions.psm1
+++ source/general-functions.psm1
@@ -60,7 +60,7 @@ Function Invoke-LogRotation {
 
     if (Test-Path -Path $logFile) {
         $time_stamp = Get-Date -Format "yyyy-MM-dd"
-        $new_log_file = "$($logFile)_$time_stamp"
+        $new_log_file = "$time_stamp-$($logFile)"
         try {
             Rename-Item -Path $logFile -NewName $new_log_file
         } catch {
