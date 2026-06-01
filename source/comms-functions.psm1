639a6a3a9f61de47c0a6139fd858b73a7ff39f6a
Jon Byrum <jtbyrum@gmail.com>
2026-05-31 22:25:01 -0700

    function name for greylog message

--- source/comms-functions.psm1
+++ source/comms-functions.psm1
@@ -38,7 +38,7 @@ function Send-GmailNotification {
 # function to send a log message to graylog using the GELF format over UDP
 # usage example:
 # Send-GraylogGelfMessage -Message "This is a test GELF message from PowerShell." -Level "6" -AdditionalFields @{ "environment" = "test"; "application" = "MyApp" } -GraylogHost "graylog.example.com" -Port 12201
-function Send-GraylogGelfMessage {
+function Send-GraylogMessage {
     param(
         [string]$GraylogHost,
         [int]$Port = 12201,
